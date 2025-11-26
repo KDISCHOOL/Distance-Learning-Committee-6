707c981e2955422bedd874e693c751aefa992f91
KDISCHOOL <97507046+KDISCHOOL@users.noreply.github.com>
2025-11-26 20:36:19 +0900

    Remove help text from admin_pin field
    Removed help text from admin_pin field in UploadFileForm.

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(required=True)
-    admin_pin = forms.CharField(required=True, max_length=10, widget=forms.PasswordInput, help_text="관리자 PIN 입력 (예: 1205)")
+    admin_pin = forms.CharField(required=True, max_length=10, widget=forms.PasswordInput)
 
 class SimpleSearchForm(forms.Form):
     name = forms.CharField(required=True, label='Korean or English name')
